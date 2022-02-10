a7a545cfa3dd037845780f210ca2bc1ad7e375c6
J <eri@Eri-2.local>
2022-02-10 23:09:55 +0900

    Added Solution1 (sort by library with shortest sign up, then by highest score)

--- src/Solution1.java
+++ src/Solution1.java
@@ -1,22 +1,38 @@
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+/**
+ * Sort each library by sign up process. Libraries with shorter sign up come first.
+ * If two libraries take the same number of days to sign up, then the one with the higher score of books comes first.
+ */
 public class Solution1 {
 
     static Answer getAnswer(Input input) {
+        List<Answer.Library> libraryList = input.getLibraryList().stream()
+                .sorted(
+                        Comparator.comparing(Library::getNumberOfDaysTakenToSignUp)
+                                .thenComparing(library -> {
+                                    long sum = library.getBooks().stream()
+                                            .mapToLong(book -> input.getBookIdScoreMap().get(book.getId()))
+                                            .sorted()
+                                            .limit(input.getNumberOfDays())
+                                            .reduce(Long::sum)
+                                            .getAsLong();
 
-        Library library1 = input.getLibraryList().get(0);
-        List<Answer.Library> libraryList = Stream.of(library1)
-                .map(library ->
-                        new Answer.Library(
-                                library.getId(),
-                                library1.getBooks().stream()
-                                        .mapToLong(Book::getId)
-                                        .boxed()
-                                        .collect(Collectors.toList())
-                        )
-                ).collect(Collectors.toList());
+                                    return sum;
+                                })
+                )
+                .map(library -> new Answer.Library(
+                        library.getId(),
+                        library.getBooks().stream()
+                                .sorted(Comparator.comparing(book -> input.getBookIdScoreMap().get(book.getId())))
+                                .limit(input.getNumberOfDays())
+                                .mapToLong(Book::getId)
+                                .boxed()
+                                .collect(Collectors.toList())
+                ))
+                .collect(Collectors.toList());
 
         return new Answer(libraryList);
     }
